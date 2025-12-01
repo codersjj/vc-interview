3f1af7ccc5276914e8e8e6cb23ea825d3e931dff
codersjj <shanewestlife@outlook.com>
2025-12-01 14:22:49 +0800

    fix: adjust server configuration for production and update Vercel rewrites

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,17 +14,23 @@ app.get('/api/books', (req, res) => {
   res.status(200).json({ msg: 'this is the books endpoint' })
 })
 
-// make our app ready for deployment
-if (ENV.NODE_ENV === 'production') {
+
+// And for development only start the server if we're not in a serverless environment
+// This will only happen locally in development mode
+if (ENV.NODE_ENV !== 'production') {
+  // Serve static files from the dist directory
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
+  // Serve index.html for all other requests
   // see: https://expressjs.com/en/guide/migrating-5.html#path-syntax
   app.get('/{*any}', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
   })
+
+  // Start the server
+  app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT))
 }
 
-app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT))
 
 // 导出 app 供 Vercel 使用
 export default app
