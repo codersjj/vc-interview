b67eeaceb5ca6cec305924336e2dbf9e26b9a77c
codersjj <shanewestlife@outlook.com>
2025-12-02 10:36:58 +0800

    fix: improve error logging during server startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,8 +31,8 @@ const startServer = async () => {
     await connectDB()
     app.listen(ENV.PORT, () => console.log('Server is running on port:', ENV.PORT))
   } catch(error) {
-    console.error('💥 Error starting the server')
+    console.error('💥 Error starting the server:', error)
+    process.exit(1)
   }
 }
-
 startServer()
