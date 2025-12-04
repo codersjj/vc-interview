516f7ac9dcd72acde509374612f864e101c963e5
codersjj <shanewestlife@outlook.com>
2025-12-04 21:04:26 +0800

    feat: auth & stream sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,5 +5,7 @@ dotenv.config({ quiet: true })
 export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
-  NODE_ENV: process.env.NODE_ENV
+  NODE_ENV: process.env.NODE_ENV,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET
 }
---
+++ backend/src/lib/stream.js
@@ -0,0 +1,33 @@
+import { StreamChat } from 'stream-chat';
+
+import { ENV } from './env.js';
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error('Stream API key or Stream API secret is missing');
+}
+
+// see: https://getstream.io/chat/docs/node/
+const serverClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async userData => {
+  try {
+    await serverClient.upsertUsers([userData]);
+    return userData
+  } catch (error) {
+    console.error('Error upserting Stream user:', error);
+  }
+}
+
+export const deleteStreamUser = async userId => {
+  try {
+    await serverClient.deleteUser(userId)
+    console.log('Stream user deleted successfully:', userId)
+  } catch (error) {
+    console.error('Error deleting Stream user:', error);
+  }
+}
+
+// todo: add another method to generate token
