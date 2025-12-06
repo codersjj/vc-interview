77b99b2973ebcbaa66efbbb41860683b18b9c020
codersjj <shanewestlife@outlook.com>
2025-12-06 17:18:49 +0800

    feat: add daisyUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <h1 className='p-10 text-3xl text-red-500 bg-amber-200'>Welcome to VC Interview</h1>
+      <button className='btn'>Default</button>
+      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-secondary">Click me</button>
       <SignedOut>
         <SignInButton mode="modal">
           <button className=''>Login</button>
