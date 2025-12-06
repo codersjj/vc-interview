2d8cfe4ae7138c2c51eae28be8f2263daa4e2971
codersjj <shanewestlife@outlook.com>
2025-12-06 22:50:25 +0800

    feat: add TanStack Query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -11,12 +12,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-      </ClerkProvider>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   SignedIn,
   SignedOut,
@@ -6,13 +7,77 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
 
 const HomePage = () => {
+  interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+  }
+
+  // fetch some data without TanStack Query
+  // const [users, setUsers] = useState<User[]>([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState<Error | null>(null);
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     setIsLoading(true);
+  //     try {
+  //       const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //       const users = await res.json();
+  //       setUsers(users);
+  //     } catch (error) {
+  //       setError(error as Error);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
+
+  //   // refetch users if error occurs
+  //   // fetch users immediately again when you focus on the window
+  //   // ...
+
+  //   getUsers();
+  // }, []);
+
+  // fetch some data with TanStack Query
+  const { isPending, error, data } = useQuery({
+    queryKey: ["users"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) {
+    return "An error has occurred: " + error.message;
+  }
+
   return (
     <div>
       <h1 className="p-10 text-3xl text-red-500 bg-amber-200">
         Welcome to VC Interview
       </h1>
+
+      {/* {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      <ul>
+        {!isLoading &&
+          !error &&
+          users.map((user: User) => <li key={user.id}>{user.name}</li>)}
+      </ul> */}
+
+      <ul>
+        {data.map((user: User) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
       <button
         className="btn"
         onClick={() => toast.success("This is a success toast!")}
